21597b362935dcc30b553b3c9f27b3a39f3b1adc
Suizhazha <573505403@qq.com>
2020-09-09 19:47:11 +0800

    初步完成导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,92 @@
 import React from 'react';
-import styled from 'styled-components'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
 
-const Box = styled.div `
- border: 1px solid red;
- min-width: 300px;
- min-height: 300px;
- margin: 20px;
- box-shadow:  0 0 3px rgba(0,0,0,0.5);
+
+const Wrapper = styled.div`
+height: 100vh;
+display: flex;
+flex-direction: column;
+`;
+
+const Main = styled.div`
+flex-grow: 1;
+overflow: auto;
 `
+const Nav = styled.nav`
+border: 1px solid blue;
+>ul{
+ display: flex;
+ >li{
+ width: 33.33333%;
+ text-align: center;
+ padding: 16px;
+ }
+}
+`
+
 
 function App() {
   return (
-    <div >
-      <Box>xxx</Box>
-    </div>
+    <Router>
+      <Wrapper>
+
+        <Main>
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from='/' to='money'/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+
+      </Wrapper>
+    </Router>
   );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch() {
+  return <h2>页面不存在</h2>;
 }
 
 export default App;
---
+++ src/components/Box.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Box = styled.div `
+ border: 1px solid red;
+ min-width: 300px;
+ min-height: 300px;
+ margin: 20px;
+ box-shadow:  0 0 3px rgba(0,0,0,0.5);
+`
+export default Box
