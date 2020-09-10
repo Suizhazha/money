c7727f02e6cc5b81c727c2ad896f1aa9db3cf1ad
Suizhazha <573505403@qq.com>
2020-09-10 21:47:38 +0800

    初步完成 money UI设计

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,41 +3,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagsSection = styled.section`
-background: #FFFFFF;
-padding: 12px 16px;
+background: #FFFFFF;padding: 12px 16px;
+  flex-grow: 1; display:flex; flex-direction: column;
+  justify-content: flex-end; align-items: flex-start;
 >ol{
 margin: 0 -12px ;
 >li{
- background: #D9D9D9;
- border-radius: 18px;
- display: inline-block;
- padding: 3px 18px;
- font-size: 14px;
- margin: 8px 12px;
+ background: #D9D9D9; border-radius: 18px;
+ display: inline-block;padding: 3px 18px;
+ font-size: 14px;margin: 8px 12px;
 }
 }
 >button{
-  background: none;
-  border: none;
-  padding: 2px 4px;
-  border-bottom: 1px solid #333;
-  color: #666;
-  margin-top: 8px;
+  background: none; border: none;
+  padding: 2px 4px;  border-bottom: 1px solid #333;
+  color: #666; margin-top: 8px;
 }
 `
 const NotesSection = styled.section`
-  background: #f5f5f5;
-  padding: 0 16px;
-  font-size: 14px;
+  background: #f5f5f5;  padding: 0 16px;  font-size: 14px;
   > label {
-    display:flex;
-    align-items: center;
+    display:flex; align-items: center;
     > span { margin-right: 16px; white-space: nowrap; }
     > input {
-      display:block;
-      width: 100%;
-      height: 72px;
-      background: none;
+      display:block; width: 100%;
+      height: 72px; background: none;
       border: none;
     }
   }
@@ -48,31 +38,74 @@ const CategorySection = styled.section`
     display:flex;
     background:#c4c4c4;
     > li {
-      width: 50%; 
-      text-align:center;
-      padding: 16px 0;
-      position:relative;
+      width: 50%;  text-align:center;
+      padding: 16px 0; position:relative;
       &.selected::after{
-        content: '';
-        display:block; 
-        height: 3px;
-        background:#333;
-        position:absolute;
-        bottom:0;
-        width: 100%;
-        left: 0;
+        content: '';  display:block; 
+        height: 3px;  background:#333;
+        position:absolute; bottom:0;
+        width: 100%;  left: 0;
       }
     }
   }
 `
-const NumberSection = styled.section`
-
+const NumberPadSection = styled.section`
+ display:flex;
+  flex-direction: column;
+  > .output{
+    background:white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align:right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+                inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .pad{ 
+    > button{
+      font-size: 18px; float: left; width: 25%; height: 64px; border: none;
+      &.ok{ height: 128px; float: right; }
+      &.zero{ width: 50%; }
+      &:nth-child(1){
+        background:#f2f2f2;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background:#E0E0E0;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background:#D3D3D3;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background:#C1C1C1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background:#B8B8B8;
+      }
+      &:nth-child(12) {
+        background:#9A9A9A;
+      }
+      &:nth-child(14) {
+        background:#A9A9A9;
+      }
+    }
+  }
+`
+const MyLayout = styled(Layout)`
+  display:flex;
+  flex-direction: column;
 `
 
 
 function Money() {
   return (
-    <Layout >
+    <MyLayout >
       <TagsSection >
         <ol>
           <li>衣</li>
@@ -94,11 +127,11 @@ function Money() {
           <li>收入</li>
         </ol>
       </CategorySection>
-      <NumberSection>
-        <div>
+      <NumberPadSection>
+        <div className="output">
           100
         </div>
-        <div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -110,13 +143,13 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
-          <button>.</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
         </div>
-      </NumberSection>
+      </NumberPadSection>
 
-    </Layout>
+    </MyLayout>
   )
 }
 
