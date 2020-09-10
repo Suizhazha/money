0a358e86098ea4005906546739a601a40ea9ca9c
Suizhazha <573505403@qq.com>
2020-09-10 21:02:16 +0800

    TagsSection,备注UI,类型UI完成

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,33 @@ import Money from './views/Money';
 import NotFound from './views/NotFound';
 import Tags from './views/Tags';
 import Statistics from './views/Statistics';
+import styled from 'styled-components';
 
 
+const AppWrapper = styled.div`
+color: #333;
+`
 function App() {
   return (
-    <Router>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from='/' to='money'/>
-            <Route path="*">
-              <NotFound/>
-            </Route>
-          </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from='/' to='money'/>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,13 +3,67 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagsSection = styled.section`
-
+background: #FFFFFF;
+padding: 12px 16px;
+>ol{
+margin: 0 -12px ;
+>li{
+ background: #D9D9D9;
+ border-radius: 18px;
+ display: inline-block;
+ padding: 3px 18px;
+ font-size: 14px;
+ margin: 8px 12px;
+}
+}
+>button{
+  background: none;
+  border: none;
+  padding: 2px 4px;
+  border-bottom: 1px solid #333;
+  color: #666;
+  margin-top: 8px;
+}
 `
 const NotesSection = styled.section`
-
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display:flex;
+    align-items: center;
+    > span { margin-right: 16px; white-space: nowrap; }
+    > input {
+      display:block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
 `
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  > ul{
+    display:flex;
+    background:#c4c4c4;
+    > li {
+      width: 50%; 
+      text-align:center;
+      padding: 16px 0;
+      position:relative;
+      &.selected::after{
+        content: '';
+        display:block; 
+        height: 3px;
+        background:#333;
+        position:absolute;
+        bottom:0;
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
 `
 const NumberSection = styled.section`
 
@@ -31,12 +85,12 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
         <ol>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ol>
       </CategorySection>
