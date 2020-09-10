bb839997040b41fe6adda4bdb1a5a97eca5bc415
Suizhazha <573505403@qq.com>
2020-09-10 15:15:13 +0800

    封装Icon组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Icon from './icon';
 
-require('icons/money.svg');
-require('icons/tag.svg');
-require('icons/statistics.svg');
+
 
 const NavWrapper = styled.nav`
 line-height: 24px;
@@ -22,7 +21,6 @@ align-items: center;
  .icon{
   width: 24px;
   height: 24px;
-
 }
  }
 }
@@ -32,21 +30,17 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <svg className='icon'>
-            <use xlinkHref='#tag'/>
-          </svg>
+          <Icon name='tag'/>
           <Link to="/tags">标签</Link>
         </li>
         <li>
-          <svg className='icon'>
-            <use xlinkHref='#money'/>
-          </svg>
+          <Icon name='money'/>
+
           <Link to="/money">记账</Link>
         </li>
         <li>
-          <svg className='icon'>
-            <use xlinkHref='#statistics'/>
-          </svg>
+          <Icon name='statistics'/>
+
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
