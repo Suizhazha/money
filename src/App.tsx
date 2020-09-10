cd0a98c032b2d0f55d6a0edfd776d37bc798c43e
Suizhazha <573505403@qq.com>
2020-09-10 15:59:23 +0800

    封装Layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,13 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
+import Layout from './components/Layout';
 
-
-const Wrapper = styled.div`
-height: 100vh;
-display: flex;
-flex-direction: column;
-`;
-
-const Main = styled.div`
-flex-grow: 1;
-overflow: auto;
-`;
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-
-        <Main>
           <Switch>
             <Route path="/tags">
               <Tags/>
@@ -42,23 +27,32 @@ function App() {
               <NoMatch/>
             </Route>
           </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
     </Router>
   );
 }
 
 function Statistics() {
-  return <h2>统计页面</h2>;
+  return (
+    <Layout >
+      <h2>统计页面</h2>
+    </Layout>
+  )
 }
 
 function Tags() {
-  return <h2>标签页面</h2>;
+  return (
+    <Layout >
+      <h2>标签页面</h2>
+    </Layout>
+  )
 }
 
 function Money() {
-  return <h2>记账页面</h2>;
+  return (
+    <Layout >
+      <h2>记账页面</h2>
+    </Layout>
+  )
 }
 
 function NoMatch() {
