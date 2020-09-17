a5ce1a53d4ff0ac82a33ced7bde1c1d403ba91d9
Suizhazha <573505403@qq.com>
2020-09-17 20:54:21 +0800

    抽离Input组件，并修改成受控组件

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,47 +1,42 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import Input from '../../components/Input';
 
 const NotesStyle = styled.section`
   background: #f5f5f5;  padding: 0 16px;  font-size: 14px;
-  > label {
-    display:flex; align-items: center;
-    > span { margin-right: 16px; white-space: nowrap; }
-    > input {
-      display:block; width: 100%;
-      height: 72px; background: none;
-      border: none;
-    }
-  }
-`
+`;
 
-type Props ={
-  value:string,
-  onChange: (value: string)=>void
+type Props = {
+  value: string,
+  onChange: (value: string) => void
 }
 
-const NoteSection:React.FC <Props> = (props)=>{
-  const note=props.value
-const refInput = useRef<HTMLInputElement>(null)
-const onBlur =()=>{
-    if (refInput.current){
-      props.onChange(refInput.current.value)
-      console.log(refInput.current.value);
-    }
-}
-return(
-  <NotesStyle>
-    <label>
-      <span>备注</span>
+const NoteSection: React.FC<Props> = (props) => {
+  const note = props.value;
+  const refInput = useRef<HTMLInputElement>(null);
+  const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+      props.onChange(e.target.value);
+  };
+  return (
+    <NotesStyle>
+      {/*修改成受控组件写法*/}
+      <Input label='备注：'
+             type='text'
+             value={note}
+             onChange={onChange}
+      >
 
-      {/* 非受控组件写法 */}
-      <input type="text" placeholder="在这里添加备注"
-             ref={refInput}
-             defaultValue={note}
-             onBlur={onBlur}
-      />
-    </label>
-  </NotesStyle>
-)
-}
+        {/*<span>备注</span>*/}
 
-export default NoteSection
+        {/*/!* 非受控组件写法 *!/*/}
+        {/*<input type="text" placeholder="在这里添加备注"*/}
+        {/*       ref={refInput}*/}
+        {/*       defaultValue={note}*/}
+        {/*       onBlur={onBlur}*/}
+        {/*/>*/}
+      </Input>
+    </NotesStyle>
+  );
+};
+
+export default NoteSection;
