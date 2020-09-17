e80df545ce8d660a72da4b87d8b290dc3e01b4e7
Suizhazha <573505403@qq.com>
2020-09-17 20:24:37 +0800

    创建单独的Button组件

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import useTags from 'useTags';
 import { useParams } from "react-router-dom";
 import Layout from 'components/Layout';
+import Icon from "components/Icon";
+import Button from "components/Button";
 
 type Params = {
   id:string
@@ -14,7 +16,19 @@ const TagEdit:React.FC = ()=>{
 const tag = findTag(Number(id))
   return(
     <Layout>
-      <div>{tag.name}</div>
+      <header>
+        <Icon name='left'/>
+        <span>编辑标签</span>
+      </header>
+
+      <label>
+        <span>标签名</span>
+        <input type="text" placeholder=""
+        />
+      </label>
+      <div>
+        <Button>删除标签</Button>
+      </div>
     </Layout>
   )
 }
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Icon from 'components/Icon';
 import { Link } from 'react-router-dom';
 import useTags from 'useTags';
+import Button from 'components/Button';
 
 const TagList = styled.ol`
   font-size: 16px; 
@@ -22,11 +23,7 @@ const TagList = styled.ol`
     }
   }
 `;
-const Button = styled.button`
-  font-size: 18px; border: none; padding: 8px 12px;
-  background: cornflowerblue; border-radius: 4px;
-  color: white;
-`;
+
 const Center = styled.div`
   display:flex;
   justify-content: center;
