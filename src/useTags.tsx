63f73f74087d116d01b0516853662caff63531bc
Suizhazha <573505403@qq.com>
2020-09-17 16:54:37 +0800

    重构：将 tag:string 重构为 tag:{id:number;name:string}

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,10 +1,13 @@
 import {useState} from 'react';
 
-const UseTags =()=>{ //封装一个自定义Hook
-  const [tags,setTags] = useState([
-   '衣','食','住','行'
+const useTags =()=>{ //封装一个自定义Hook
+  const [tags,setTags] = useState<{id:number;name:string}[]>([
+    {id:1,name:'衣'},
+    {id:2,name:'食'},
+    {id:3,name:'住'},
+    {id:4,name:'行'}
     ])
  return{tags,setTags}
 }
 
-export default UseTags
+export default useTags
