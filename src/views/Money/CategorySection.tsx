006708d44a7b708477ad159476ceb267f00749ea
Suizhazha <573505403@qq.com>
2020-09-21 14:40:32 +0800

    支出收入背景可以单独设置

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 
 const CategoryStyle= styled.section`
   font-size: 24px;
-  > ol{
+  > ul{
     display:flex;
-    background:#c4c4c4;
     > li {
       width: 50%;  text-align:center;
       padding: 16px 0; position:relative;
@@ -28,7 +27,7 @@ const CategorySection:React.FC <Props > = (props)=>{
   const category = props.value
   return(
 <CategoryStyle>
-  <ol>
+  <ul>
     <li className={category==='-'?'selected':''}
     onClick={()=>{
       props.onChange('-')
@@ -39,7 +38,7 @@ const CategorySection:React.FC <Props > = (props)=>{
           props.onChange('+')
         }}
     >收入</li>
-  </ol>
+  </ul>
 </CategoryStyle>
   )
 }
